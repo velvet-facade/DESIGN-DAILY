b3b771123a5a216c7e1b52b7120cac2430f333f2
velvet-facade <104369167+velvet-facade@users.noreply.github.com>
2022-06-24 00:41:55 +0530

    need to overwrite repository

--- 1572/sketch.js
+++ 1572/sketch.js
@@ -1,1 +1,4 @@
-const projectData = fetch('https://uidesigndaily.com/posts/figma-shopping-cart-online-shop-card-day-1572')   
+const projectData = fetch('https://uidesigndaily.com/posts/figma-shopping-cart-online-shop-card-day-1572');
+
+//NEED TO OVERRITE GITHUB REPOSITORY
+const masterRepository = fetch('https://github.com/velvet-facade/DESIGN-DAILY-MASTER');
